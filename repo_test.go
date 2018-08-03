58026cca98288f8ef4838be1836ea1a039438eb3
fsamin <francois.samin+github@gmail.com>
2018-08-03 13:55:51 +0200

    skip push test on travis

--- repo_test.go
+++ repo_test.go
@@ -279,6 +279,9 @@ func TestCheckoutNewBranch_Checkout_DeleteBranch(t *testing.T) {
 }
 
 func TestPush(t *testing.T) {
+	if os.Getenv("TRAVIS_BUILD_DIR") != "" {
+		t.SkipNow()
+	}
 	path := filepath.Join("testdata", "TestPush")
 	assert.NoError(t, os.MkdirAll(path, os.FileMode(0755)))
 	defer os.RemoveAll("testdata")
